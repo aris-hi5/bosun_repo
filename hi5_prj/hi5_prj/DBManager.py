6d26bcdeaef0a79fbd4fdb8b3008cc5bad7ee5f4
jangjh0201 <jangjh0201@gmail.com>
2024-08-23 17:39:06 +0900

    Mod : Table 사용 요청 추가

--- hi5_prj/hi5_prj/DBManager.py
+++ hi5_prj/hi5_prj/DBManager.py
@@ -7,6 +7,7 @@ from hi5_message.srv import OrderCall
 # url = "http://192.168.1.8:8000/kiosk"
 kiosk = "http://172.30.1.5:8000/kiosk"
 robot = "http://172.30.1.5:8000/robot"
+table = "http://172.30.1.5:8000/table"
 
 
 class DBManagerNode(Node):
@@ -14,7 +15,9 @@ class DBManagerNode(Node):
         super().__init__('db_manager_node')
         self.kiosk = kiosk
         self.robot = robot
-        self.order_call_db_service = self.create_service(OrderCall, "order_call_db", self.ordercall_callback)
+        self.table = table
+        self.order_call_db_service = self.create_service(
+            OrderCall, "order_call_db", self.ordercall_callback)
 
     def ordercall_callback(self, req, res):
         try:
@@ -23,24 +26,65 @@ class DBManagerNode(Node):
                 self.get_logger().info(f"{req.data}")
                 self.get_logger().info("OrderCall Service !!")
                 if "OR" in req.data:
-                    headers = {"Content-Type": "application/json", "dataType": "json"}
-                    response = requests.post(self.kiosk, headers=headers, data=req.data)
+                    headers = {"Content-Type": "application/json",
+                               "dataType": "json"}
+                    response = requests.post(
+                        url=self.kiosk, headers=headers, data=req.data)
                     self.get_logger().info(f"{response}")
                     self.get_logger().info(f"ororororor{req.data}")
                     # self.get_logger().info(f"hihihihi{response}")
-                    self.get_logger().info(f"ororororo{response.json()}") # json형태
+                    self.get_logger().info(
+                        f"ororororo{response.json()}")  # json형태
                     message = response.json()
-                    self.get_logger().info(f"responseresponseresponse{message}")
+                    self.get_logger().info(
+                        f"responseresponseresponse{message}")
 
-                    message = json.dumps(message) #dict -> string
+                    # object(dict) -> string(json)
+                    message = json.dumps(message)
 
                     res.success = True
                     res.message = message
                     return res
+                elif "TR" in req.data:
+                    headers = {"Content-Type": "application/json",
+                               "dataType": "json"}
+                    data = json.loads(req.data)
 
+                    # 필요한 정보를 추출
+                    tr_data = data["TR"]
+                    id_value = tr_data["id"]
+                    status_value = tr_data["status"]
+
+                    # URL을 생성
+                    self.table = self.table + f'/{id_value}'
+
+                    # 요청에 사용할 JSON 데이터
+                    json_data = {"status": status_value}
+
+                    # PATCH 요청
+                    response = requests.patch(
+                        url=self.table, headers=headers, data=json_data)
+
+                    self.get_logger().info(f"{response}")
+                    self.get_logger().info(f"trtrtrtrtr{req.data}")
+                    # self.get_logger().info(f"hihihihi{response}")
+                    self.get_logger().info(
+                        f"trtrtrtr{response.json()}")  # json형태
+                    message = response.json()
+                    self.get_logger().info(
+                        f"responseresponseresponse{message}")
+
+                    # object(dict) -> string(json)
+                    message = json.dumps(message)
+
+                    res.success = True
+                    res.message = message
+                    return res
                 elif "OS" in req.data:
-                    headers = {"Content-Type": "application/json", "dataType": "json"}
-                    response = requests.post(self.robot, headers=headers, data=req.data)
+                    headers = {"Content-Type": "application/json",
+                               "dataType": "json"}
+                    response = requests.post(
+                        url=self.robot, headers=headers, data=req.data)
 
                     # self.get_logger().info(f"{response}")
                     # self.get_logger().info(f"osososososo{req.data}")
@@ -72,12 +116,14 @@ class DBManagerNode(Node):
             res.message = f"Unexpected error: {e}"
             return res
 
+
 def main(args=None):
     rclpy.init(args=args)
     node = DBManagerNode()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
